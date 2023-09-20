f613efba746d13cf9179434df812ee4961f82acd
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-09-20 21:06:41 +0300

    Update write_functions.c

--- write_functions.c
+++ write_functions.c
@@ -1,70 +1,70 @@
+
 #include "main.h"
 
-// Custom _write_str function for string formatting
-void _write_str(const char *str);
-// Custom _write_int function for formatting integers
-void _write_int(int num);
-// Custom _write_char function for writing a single character
-void _write_char(char c);
-// Custom _write_newline function for writing a newline character
-void _write_newline();
-// Custom _startswith function
-int _startswith(const char *str, const char *prefix);
 
-
-// Custom _write_str function for string formatting
-void _write_str(const char *str) {
-    write(STDOUT_FILENO, str, _strlen(str));
+/**
+ * _write_str - Writes a string to standard output.
+ *
+ * @str: The string to be written.
+ */
+void _write_str(const char *str)
+{
+	write(STDOUT_FILENO, str, _strlen(str));
 }
 
-// Custom _write_int function for formatting integers
-void _write_int(int num) {
-    char buffer[20]; // Assuming a reasonable buffer size
-    int length = 0;
-    int temp = num;
+/**
+ * _write_int - Writes an integer to standard output.
+ *
+ * @num: The integer to be written.
+ */
+void _write_int(int num)
+{
+	char buffer[20]; /* Assuming a reasonable buffer size */
+	int length = 0;
+	int temp = num;
 
-    // Handle negative numbers
-    if (temp < 0) {
-        _write_char('-');
-        temp = -temp;
-    }
+	/* Handle negative numbers */
+	if (temp < 0)
+	{
+		_write_char('-');
+		temp = -temp;
+	}
 
-    // Convert the integer to a string in reverse order
-    do {
-        buffer[length++] = temp % 10 + '0';
-        temp /= 10;
-    } while (temp > 0);
+	/* Convert the integer to a string in reverse order */
+	do {
+		buffer[length++] = temp % 10 + '0';
+		temp /= 10;
+	} while (temp > 0);
 
-    // Reverse the string to get the correct order
-    for (int i = 0, j = length - 1; i < j; i++, j--) {
-        char tmp = buffer[i];
-        buffer[i] = buffer[j];
-        buffer[j] = tmp;
-    }
+	/* Reverse the string to get the correct order */
+	for (int i = 0, j = length - 1; i < j; i++, j--)
+	{
+		char tmp = buffer[i];
 
-    write(STDOUT_FILENO, buffer, length);
+		buffer[i] = buffer[j];
+		buffer[j] = tmp;
+	}
+
+	write(STDOUT_FILENO, buffer, length);
 }
 
 
-// Custom _write_char function for writing a single character
-void _write_char(char c) {
-    write(STDOUT_FILENO, &c, 1);
+/**
+ * _write_char - Writes a single character to standard output.
+ *
+ * @c: The character to be written.
+ */
+void _write_char(char c)
+{
+	write(STDOUT_FILENO, &c, 1);
 }
 
-// Custom _write_newline function for writing a newline character
-void _write_newline() {
-    char newline = '\n';
-    write(STDOUT_FILENO, &newline, 1);
-}
+/**
+ * _write_newline - Writes a newline character to standard output.
+ */
+void _write_newline(void)
+{
+	char newline = '\n';
 
-// Custom _startswith function
-int _startswith(const char *str, const char *prefix) {
-    while (*prefix) {
-        if (*prefix != *str) {
-            return 0; // Does not start with the prefix
-        }
-        prefix++;
-        str++;
-    }
-    return 1; // Starts with the prefix
+	write(STDOUT_FILENO, &newline, 1);
 }

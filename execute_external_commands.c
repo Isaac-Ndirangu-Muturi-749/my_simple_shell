6f4665a13ae22625facb33a4c65b61041928948a
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-09-20 21:06:58 +0300

    Update execute_external_commands.c

--- execute_external_commands.c
+++ execute_external_commands.c
@@ -1,69 +1,97 @@
 #include "main.h"
 
-// Function to find the path of an external command
-char* find_command_path(char *command);
-// Function to execute an external command
-void execute_external_command(char **args, int arg_count, int *last_exit_status);
+/**
+ * find_command_path - Finds the path of an external command.
+ *
+ * @command: The name of the command to find.
+ *
+ * Return: A pointer to the command's path, or NULL if not found.
+ */
+char *find_command_path(char *command)
+{
+	/* Check if the command exists in one of the directories in PATH */
+	char *path = _getenv("PATH");
+	char *path_copy = _strdup(path); /* Make a copy of PATH */
+	char *dir = _strtok(path_copy, ":");
+	char *command_path = NULL;
 
-// Function to find the path of an external command
-char* find_command_path(char *command) {
-    // Check if the command exists in one of the directories in PATH
-    char *path = _getenv("PATH");
-    char *path_copy = _strdup(path); // Make a copy of PATH
-    char *dir = _strtok(path_copy, ":");
-    char *command_path = NULL;
+	while (dir != NULL)
+	{
+		/* Construct the full path to the command */
+		char full_path[MAX_INPUT_SIZE];
 
-    while (dir != NULL) {
-        // Construct the full path to the command
-        char full_path[MAX_INPUT_SIZE];
-        _write_str(dir); _write_char('/'); _write_str(command);
+		_write_str(dir);
+		_write_char('/');
+		_write_str(command);
 
-        // Check if the command exists in this directory
-        if (access(full_path, X_OK) == 0) {
-            // Command exists, set command_path and break
-            command_path = _strdup(full_path);
-            break;
-        }
+		/* Check if the command exists in this directory */
+		if (access(full_path, X_OK) == 0)
+		{
+			/* Command exists, set command_path and break */
+			command_path = _strdup(full_path);
+			break;
+		}
 
-        dir = _strtok(NULL, ":");
-    }
+		dir = _strtok(NULL, ":");
+	}
 
-    free(path_copy);
-    return command_path;
+	free(path_copy);
+	return (command_path);
 }
 
-// Function to execute an external command
-void execute_external_command(char **args, int arg_count, int *last_exit_status) {
-    // Fork a child process
-    pid_t pid = fork();
 
-    if (pid == 0) {
-        // Child process
-        char *command_path = find_command_path(args[0]);
+/**
+ * execute_external_command - Executes an external command.
+ *
+ * @args:             An array of strings representing the command arguments.
+ * @arg_count:        The number of arguments in the array.
+ * @last_exit_status: Pointer to an integer containing the last exit status.
+ */
+void execute_external_command(char **args, int arg_count, int *last_exit_status)
+{
+	/* Fork a child process */
+	pid_t pid = fork();
 
-        if (command_path != NULL) {
-            // Execute the command using the full path
-            char *const command_argv[] = {command_path, args[0], args[1], args[2], NULL}; // Adjust the number of arguments as needed
-            if (execve(command_path, command_argv, environ) == -1) {
-                perror("execve");
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            // Command not found in PATH
-            char msg[MAX_INPUT_SIZE];
-            _write_str("Command not found: "); _write_str(args[0]); _write_newline();
-            write(STDERR_FILENO, msg, _strlen(msg));
-            exit(EXIT_FAILURE);
-        }
-    } else if (pid < 0) {
-        perror("fork");
-    } else {
-        // Parent process
-        int status;
-        waitpid(pid, &status, 0);
+	if (pid == 0)
+	{
+		/* Child process */
+		char *command_path = find_command_path(args[0]);
 
-        if (WIFEXITED(status)) {
-            *last_exit_status = WEXITSTATUS(status);
-        }
-    }
+		if (command_path != NULL)
+		{
+			/* Execute the command using the full path */
+			/* Adjust the number of arguments as needed*/
+			char *const command_argv[] = {command_path, args[0], args[1], args[2], NULL};
+
+			if (execve(command_path, command_argv, environ) == -1)
+			{
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+		} else
+		{
+			/* Command not found in PATH */
+			char msg[MAX_INPUT_SIZE];
+
+			_write_str("Command not found: ");
+			_write_str(args[0]);
+			_write_newline();
+			write(STDERR_FILENO, msg, _strlen(msg));
+			exit(EXIT_FAILURE);
+		}
+	} else if (pid < 0)
+	{
+		perror("fork");
+	} else
+	{
+		/* Parent process */
+		int status;
+
+		waitpid(pid, &status, 0);
+
+		if (WIFEXITED(status))
+		{
+			*last_exit_status = WEXITSTATUS(status);
+		}
+	}
 }

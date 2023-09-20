8a033f72651e4678f91132632a5fdc990b704526
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-09-20 21:07:09 +0300

    Update execute_handle_variable_replacement.c

--- execute_handle_variable_replacement.c
+++ execute_handle_variable_replacement.c
@@ -1,31 +1,54 @@
 #include "main.h"
 
-// Function to handle variable replacement in arguments
-void handle_variable_replacement(char **args, int arg_count, int *last_exit_status, pid_t shell_pid);
+/**
+ * handle_variable_replacement - Handles variable replacement in command
+ * arguments.
+ *
+ * @args:             An array of strings representing the command arguments.
+ * @arg_count:        The number of arguments in the array.
+ * @last_exit_status: Pointer to an integer containing the last exit status.
+ * @shell_pid:        The process ID of the shell.
+ *
+ * Description: This function takes an array of command arguments and searches
+ * for special variables (e.g., "$?", "$$") to replace with their corresponding
+ * values. It also updates the last exit status. The modified arguments are
+ * stored back in the 'args' array.
+ */
+void handle_variable_replacement(char **args, int arg_count, int *last_exit_status, pid_t shell_pid)
+{
+	for (int j = 0; j < arg_count; j++)
+	{
+		if (_strcmp(args[j], "$?") == 0)
+		{
+			/* Replace $? with the last exit status */
+			/* Assuming exit status can be represented in 10 digits */
+			char exit_status_str[10];
 
-// Function to handle variable replacement in arguments
-void handle_variable_replacement(char **args, int arg_count, int *last_exit_status, pid_t shell_pid) {
-    for (int j = 0; j < arg_count; j++) {
-        if (_strcmp(args[j], "$?") == 0) {
-            // Replace $? with the last exit status
-            char exit_status_str[10]; // Assuming exit status can be represented in 10 digits
-            _write_int(*last_exit_status);
-            char *new_arg = _strdup(exit_status_str);
-            if (new_arg == NULL) {
-                perror("malloc");
-                exit(EXIT_FAILURE);
-            }
-            args[j] = new_arg;
-        } else if (_strcmp(args[j], "$$") == 0) {
-            // Replace $$ with the shell's PID
-            char shell_pid_str[10]; // Assuming PID can be represented in 10 digits
-            _write_int(shell_pid);
-            char *new_arg = _strdup(shell_pid_str);
-            if (new_arg == NULL) {
-                perror("malloc");
-                exit(EXIT_FAILURE);
-            }
-            args[j] = new_arg;
-        }
-    }
+			_write_int(*last_exit_status);
+			char *new_arg = _strdup(exit_status_str);
+
+			if (new_arg == NULL)
+			{
+				perror("malloc");
+				exit(EXIT_FAILURE);
+			}
+			args[j] = new_arg;
+		}
+		else if (_strcmp(args[j], "$$") == 0)
+		{
+			/* Replace $$ with the shell's PID */
+			/* Assuming PID can be represented in 10 digits */
+			char shell_pid_str[10];
+
+			_write_int(shell_pid);
+			char *new_arg = _strdup(shell_pid_str);
+
+			if (new_arg == NULL)
+			{
+				perror("malloc");
+				exit(EXIT_FAILURE);
+			}
+			args[j] = new_arg;
+		}
+	}
 }
